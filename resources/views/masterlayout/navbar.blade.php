d2bfbfcb4213cd9c194e4044e546981299fe9f2d
Ansh16012000 <anshverma242@gmail.com>
2021-06-21 14:04:21 +0530

    Tried using template bootstrap css in our navbar and footer
    Tried using template bootstrap css

--- resources/views/masterlayout/navbar.blade.php
+++ resources/views/masterlayout/navbar.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>STORE-LOCATOR</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/navbar.css') }}">
 </head>
 <body>
